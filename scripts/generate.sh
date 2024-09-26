1f688079a24dc96fe783172ad8879a6b64907c12
Jason Song <i@wolfogre.com>
2024-09-26 15:34:00 +0800

    feat: improve scripts

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -2,23 +2,38 @@
 
 set -euo pipefail
 
-rm -f main.go
+rm -f main.go.tmp
 
 write() {
-  echo "$@" >> main.go
+  echo "$@" >> main.go.tmp
 }
 
 write "package main"
 write "import ("
-cat list.txt | while read -r MODULE; do
-  write "  _ \"$MODULE\""
+
+# make sure the network is available
+curl -f -s "https://proxy.golang.org/cached-only/" -o /dev/null
+
+find "$GOPATH/pkg/mod" -name "go.mod" | while read -r MOD_FILE; do
+  MODULE=$(grep -E "^module" "$MOD_FILE" | awk '{print $2}' | tr -d '"')
+  echo "found $MODULE" >&2
+  echo "$MODULE"
+done | sort -u | while read -r MODULE; do
+  ESCAPED_MODULE=$(echo "$MODULE" | sed 's/[A-Z]/!&/g' | tr '[:upper:]' '[:lower:]')
+  if curl -f -s "https://proxy.golang.org/cached-only/$ESCAPED_MODULE/@v/list" -o /dev/null; then
+    echo "write $MODULE" >&2
+    write "  _ \"$MODULE\""
+  else
+    # make sure the module is public
+    echo "exclude $MODULE" >&2
+    continue
+  fi
 done
+
 write ")"
 write "func main() {"
 write "println(\"Thanks for your hard work, Dependabot!\")"
 write "}"
 
+mv main.go.tmp main.go
 go fmt .
-
-go mod edit -go="$(go version | awk '{print $3}' | cut -c 3- | grep -oE '^\d+\.\d+')"
-go mod tidy
