e59de5a6f43c1b248af110e204c1cfacde949a75
Jason Song <i@wolfogre.com>
2024-10-08 15:43:39 +0800

    chore: check internal

--- scripts/importable.go
+++ scripts/importable.go
@@ -3,12 +3,24 @@ package main
 import (
 	"go/build"
 	"os"
+	"strings"
 )
 
 func main() {
 	name := os.Args[1]
 	pkg, err := build.Import(name, "", build.ImportComment)
-	if err == nil && pkg.Name == "main" {
+	if err != nil {
+		println(err.Error())
 		os.Exit(1)
+	}
+	if pkg.Name == "main" {
+		println("package is main")
+		os.Exit(1)
+	}
+	for _, v := range strings.Split(pkg.ImportPath, "/") {
+		if v == "internal" {
+			println("package is internal")
+			os.Exit(1)
+		}
 	}
 }
