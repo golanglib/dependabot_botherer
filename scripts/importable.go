107e47d78d1c12bb9ebc2a60d010b166a61a5e21
Jason Song <i@wolfogre.com>
2024-10-09 10:13:05 +0800

    fix: improve importable.go

--- scripts/importable.go
+++ scripts/importable.go
@@ -1,26 +1,43 @@
 package main
 
 import (
+	"fmt"
 	"go/build"
 	"os"
+	"path/filepath"
 	"strings"
+
+	"golang.org/x/mod/modfile"
 )
 
 func main() {
 	name := os.Args[1]
-	pkg, err := build.Import(name, "", build.ImportComment)
-	if err != nil {
-		println(err.Error())
+
+	exit := func(msg string) {
+		_, _ = fmt.Fprintf(os.Stderr, "%s: %s\n", name, msg)
 		os.Exit(1)
 	}
+
+	pkg, err := build.Import(name, "", build.ImportComment)
+	if err != nil {
+		exit(err.Error())
+	}
 	if pkg.Name == "main" {
-		println("package is main")
-		os.Exit(1)
+		exit("package is main")
 	}
 	for _, v := range strings.Split(pkg.ImportPath, "/") {
 		if v == "internal" {
-			println("package is internal")
-			os.Exit(1)
+			exit("package is internal")
+		}
+	}
+	modFile := filepath.Join(pkg.Dir, "go.mod")
+	if _, err := os.Stat(modFile); err == nil {
+		if data, err := os.ReadFile(modFile); err == nil {
+			if mod, err := modfile.Parse(modFile, data, nil); err == nil {
+				if len(mod.Replace) > 0 {
+					exit("package has replace")
+				}
+			}
 		}
 	}
 }
