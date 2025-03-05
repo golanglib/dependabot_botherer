bec0289c8794cd8af81ec1a00c8068913d5bcd6a
Jason Song <i@wolfogre.com>
2025-03-05 18:31:18 +0800

    fix: pseudo

--- scripts/pseudo.sh
+++ scripts/pseudo.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 
 awk '/v[0-9]+\.[0-9]+\.[0-9]+(-|-pre\.0\.|-0\.)[0-9]{14}-[0-9a-f]{12}/' go.mod
 
-awk '!/v[0-9]+\.[0-9]+\.[0-9]+(-|-pre\.0\.|-0\.)[0-9]{14}-[0-9a-f]{12}/' go.mod go.mod > go.mod.tmp
+awk '!/v[0-9]+\.[0-9]+\.[0-9]+(-|-pre\.0\.|-0\.)[0-9]{14}-[0-9a-f]{12}/' go.mod > go.mod.tmp
 
 mv go.mod.tmp go.mod
